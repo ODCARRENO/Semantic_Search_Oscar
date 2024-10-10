352fa8b3cf443144e59cc3035a8b7adf5944d559
ODCARRENO <osckar.car@gmail.com>
2024-10-09 19:07:54 -0500

    comentario de los cambios realizados

--- main_students.py
+++ main_students.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 def main(query):
-    df = pd.read_csv('./IMDB top 1000.csv')
+    df = pd.read_csv('./IMDB_top_1000.csv')
     print(df.head())
     # TODO: Completar esta función para realizar búsquedas semánticas con base en el código del archivo test.ipynb
 
 
 
 if __name__ == '__main__':
-    query = input('ingresa el termino de busqueda')
+    query = input('ingresa el termino de busqueda: ')
     main(query)
